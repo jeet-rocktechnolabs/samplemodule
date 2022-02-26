8b70637547b76837191bc86bcc1142533bf6b7f4
Jeet <jeet@rocktechnolabs.com>
2022-02-26 16:18:01 +0530

    Ticket #123 , Fixing contoller issue

--- Controller/Adminhtml/Index/Index.php
+++ Controller/Adminhtml/Index/Index.php
@@ -7,6 +7,7 @@ class Index extends \Magento\Backend\App\Action
 {
     public function execute()
     {
+        ehco "test23232323232";exit;
         $this->_view->loadLayout();
         $this->_view->getLayout()->initMessages();
         $this->_view->renderLayout();
