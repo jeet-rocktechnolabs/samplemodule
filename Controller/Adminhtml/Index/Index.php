5a0febdba143013bb6a4eaf981f9ea6ae416362a
Jeet <jeet@rocktechnolabs.com>
2022-02-26 16:36:07 +0530

    trying to generate conflict

--- Controller/Adminhtml/Index/Index.php
+++ Controller/Adminhtml/Index/Index.php
@@ -8,6 +8,8 @@ class Index extends \Magento\Backend\App\Action
     public function execute()
     {
         ehco "test23232323232";exit;
+        // adding code for conflict
+        ehco "Jeet Hello";exit;
         $this->_view->loadLayout();
         $this->_view->getLayout()->initMessages();
         $this->_view->renderLayout();
