ee6e2a16677f61cb1399d39a6e8d6ce04edc5d5b
Jeet <jeet@rocktechnolabs.com>
2022-02-26 16:21:24 +0530

    Pushing all files

--- Controller/Index/Index.php
+++ Controller/Index/Index.php
@@ -7,6 +7,8 @@ class Index extends \Magento\Framework\App\Action\Action
     public function execute()
     {
 
+        echo "test"; exit;
+
         $this->_view->loadLayout();
         $this->_view->getLayout()->initMessages();
         $this->_view->renderLayout();
